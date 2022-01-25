0f036095eef088b721fb6231b1c9c458fb1f0ce8
imsourceboom <ecmascript.developer@gmail.com>
2022-01-25 21:12:11 +0900

    update scripts

--- scripts/operator-stake.sh
+++ scripts/operator-stake.sh
@@ -4,27 +4,26 @@ source "$HOME/ton-scripts/scripts/env.sh"
 
 mytonctrl <<< "set stake 0"
 
-CURRENT_TIME=$(date +%s)
 ELECTION_STATE=$($SCRIPTS_DIR/ton-election-state.sh)
-ELECTION_START=$($SCRIPTS_DIR/ton-election-start.sh)
-ELECTION_END=$($SCRIPTS_DIR/ton-election-end.sh)
-PARTICIPATE=$($SCRIPTS_DIR/ton-participate-state.sh)
-
 if [ $ELECTION_STATE != "ACTIVE" ]; then
 	echo "NOT ELECTION"
 	exit
 fi
 
+CURRENT_TIME=$(date +%s)
+ELECTION_START=$($SCRIPTS_DIR/ton-election-start.sh)
 if [ $(($CURRENT_TIME - $ELECTION_START)) -lt 100 ]; then
 	echo "DONT VOTE YET"
 	exit
 fi
 
+ELECTION_END=$($SCRIPTS_DIR/ton-election-end.sh)
 if [ $CURRENT_TIME -gt $ELECTION_END ]; then
 	echo "ELECTION END"
 	exit
 fi
 
+PARTICIPATE=$($SCRIPTS_DIR/ton-participate-state.sh)
 if [ $PARTICIPATE = "ACTIVE" ]; then
 	echo "Already Participated"
 	exit
