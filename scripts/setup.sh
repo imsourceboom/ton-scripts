1fd7f3ebe5b73b876f124d5c6305ddf8a73f2778
imsourceboom <ecmascript.developer@gmail.com>
2022-01-25 11:08:08 +0900

    update setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -i
 
 cd $HOME
 
@@ -14,12 +14,12 @@ sudo apt update -y && sudo apt install git wget curl jq bc -y
 # mytonctrl install
 wget https://raw.githubusercontent.com/igroman787/mytonctrl/master/scripts/install.sh
 sudo bash install.sh -m full
-sudo chmod 777 /var/ton-work/db/config.json
+#sudo chmod 777 /var/ton-work/db/config.json
 mytonctrl <<< exit
 rm $HOME/install.sh
 
 cp $HOME/ton-scripts/configs/bash.config $HOME/.bashrc
-source "$HOME/.bashrc"
+source $HOME/.bashrc
 
 crontab $HOME/ton-scripts/configs/cron.config
 crontab -l
