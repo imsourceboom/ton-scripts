6f940a51a47380376fe7f1199cc759d5fa4e2947
imsourceboom <ecmascript.developer@gmail.com>
2022-09-15 20:50:24 +0900

    modified clean log script

--- scripts/clean-log.sh
+++ scripts/clean-log.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-OLD_DB=$(./ton-remain-disk.sh | tr -d 'G')
+. env.sh
+
+OLD_DB=$($SCRIPTS_DIR/ton-remain-disk.sh | tr -d 'G')
 echo 'clean' | sudo tee /var/ton-work/log*
 echo "OLD SPACE: ${OLD_DB}G"
 sleep 5
-NEW_DB=$(./ton-remain-disk.sh | tr -d 'G')
+NEW_DB=$($SCRIPTS_DIR/ton-remain-disk.sh | tr -d 'G')
 SECURED=$(($NEW_DB - $OLD_DB))
 echo "NEW SPACE: ${NEW_DB}G"
 echo "Secured storage space as much as ${SECURED}G"
