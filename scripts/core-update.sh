f398c6d1e98db3e1687ae67e89b8738b0f01b6c6
imsourceboom <ecmascript.developer@gmail.com>
2022-09-24 17:04:29 +0900

    update core update scirpt

--- scripts/core-update.sh
+++ scripts/core-update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 MYTONCORE="/usr/src/mytonctrl/mytoncore.py"
+TMP_CORE="/tmp/mytoncore.py"
 
-sed -i "/sp = stakePercent \/ 100/a\ \t\t\tdeduct = random.randint(4, 8)" $MYTONCORE
-sed -i "/stake = int(account.balance\*sp/a\ \t\t\t\tstake = account.balance - deduct" $MYTONCORE
-sed -i "s/stake = int(account.balance\*sp\/2)/#stake = int(account.balance\*sp\/2)/" $MYTONCORE
-sed -i "s/stake = int(account.balance\*sp)/#stake = int(account.balance\*sp)/" $MYTONCORE
+sudo cp $MYTONCORE /tmp
+
+sed -i "/sp = stakePercent \/ 100/a\ \t\t\tdeduct = random.randint(4, 8)" $TMP_CORE
+sed -i "/stake = int(account.balance\*sp/a\ \t\t\t\tstake = account.balance - deduct" $TMP_CORE
+sed -i "s/stake = int(account.balance\*sp\/2)/#stake = int(account.balance\*sp\/2)/" $TMP_CORE
+sed -i "s/stake = int(account.balance\*sp)/#stake = int(account.balance\*sp)/" $TMP_CORE
+
+cat $TMP_CORE > $MYTONCORE
 
 cat $MYTONCORE | grep 'deduct'
 sudo systemctl restart mytoncore.service
