ec6781ee4693c4709a22848e7d26ac5c57b11a12
imsourceboom <ecmascript.developer@gmail.com>
2022-09-24 17:10:23 +0900

    update core update scirpt

--- scripts/core-update.sh
+++ scripts/core-update.sh
@@ -4,13 +4,14 @@ MYTONCORE="/usr/src/mytonctrl/mytoncore.py"
 TMP_CORE="/tmp/mytoncore.py"
 
 sudo cp $MYTONCORE /tmp
+sudo chown $USER:$USER $TMP_CORE
 
 sed -i "/sp = stakePercent \/ 100/a\ \t\t\tdeduct = random.randint(4, 8)" $TMP_CORE
 sed -i "/stake = int(account.balance\*sp/a\ \t\t\t\tstake = account.balance - deduct" $TMP_CORE
 sed -i "s/stake = int(account.balance\*sp\/2)/#stake = int(account.balance\*sp\/2)/" $TMP_CORE
 sed -i "s/stake = int(account.balance\*sp)/#stake = int(account.balance\*sp)/" $TMP_CORE
 
-cat $TMP_CORE > $MYTONCORE
+cat $TMP_CORE | sudo tee $MYTONCORE
 
 cat $MYTONCORE | grep 'deduct'
 sudo systemctl restart mytoncore.service
