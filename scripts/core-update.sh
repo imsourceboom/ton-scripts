cd64668cf4db90af6944b6fab933d1fea3fb9f8e
imsourceboom <ecmascript.developer@gmail.com>
2022-09-24 18:24:14 +0900

    update core update scirpt

--- scripts/core-update.sh
+++ scripts/core-update.sh
@@ -6,8 +6,8 @@ TMP_CORE="/tmp/mytoncore.py"
 sudo cp $MYTONCORE /tmp
 sudo chown $USER:$USER $TMP_CORE
 
-
-sed -i "/sp = stakePercent \/ 100/a\                        deduct = random.randint(4, 8)" $TMP_CORE
+#sed -i "/sp = stakePercent \/ 100/a\                        deduct = random.randint(4, 8)" $TMP_CORE
+sed -i "/sp = stakePercent \/ 100/a\ \t\t\tdeduct = random.randint(4, 8)" $TMP_CORE
 sed -i "/stake = int(account.balance\*sp/a\                                stake = account.balance - deduct" $TMP_CORE
 sed -i "s/stake = int(account.balance\*sp\/2)/#stake = int(account.balance\*sp\/2)/" $TMP_CORE
 sed -i "s/stake = int(account.balance\*sp)/#stake = int(account.balance\*sp)/" $TMP_CORE
